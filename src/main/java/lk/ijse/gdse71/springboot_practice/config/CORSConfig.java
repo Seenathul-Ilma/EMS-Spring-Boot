b95aa22df00e25066b762b6723d2eb5db25e6bbc
ZeenathulIlma <seenathulilma121243@gmail.com>
2025-07-16 12:49:19 +0530

    add CORS configuration for handling cross-origin requests

---
+++ src/main/java/lk/ijse/gdse71/springboot_practice/config/CORSConfig.java
@@ -0,0 +1,33 @@
+package lk.ijse.gdse71.springboot_practice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * --------------------------------------------
+ * Author: Zeenathul Ilma
+ * GitHub: https://github.com/Seenathul-Ilma
+ * Website: https://zeenathulilma.vercel.app/
+ * --------------------------------------------
+ * Created: 7/16/2025 12:44 PM
+ * Project: SpringBoot_Practice
+ * --------------------------------------------
+ **/
+
+@Configuration
+public class CORSConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(org.springframework.web.servlet.config.annotation.CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:63342") // Allow 63342 origins  - If you want to allow all origins, use "*"
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allow specific HTTP methods
+                        .allowedHeaders("*") // Allow all headers
+                        .allowCredentials(true); // Allow credentials
+            }
+        };
+    }
+}
